a52ee0367566995b3a794eb27bb60551a9a55f88
leahgrace555 <leah.grace555@gmail.com>
2020-11-24 17:55:37 -0500

    refactoring

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,38 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
 
 import './todo.scss';
 
-class ToDo extends React.Component {
+function ToDo(props) {
+  useEffect( () => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-    };
-  }
-
-  addItem = (item) => {
-    item._id = Math.random();
-    item.complete = false;
-    this.setState({ list: [...this.state.list, item]});
-  };
-
-  toggleComplete = id => {
-
-    let item = this.state.list.filter(i => i._id === id)[0] || {};
-
-    if (item._id) {
-      item.complete = !item.complete;
-      let list = this.state.list.map(listItem => listItem._id === item._id ? item : listItem);
-      this.setState({list});
-    }
-
-  };
-
-  componentDidMount() {
-    let list = [
+    let listStuff = [
       { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
       { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
       { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
@@ -40,34 +15,64 @@ class ToDo extends React.Component {
       { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
     ];
 
-    this.setState({list});
-  }
+    setList(listStuff);
+  }, []) 
 
-  render() {
+  const [list, setList] = useState( [] );
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     list: [],
+  //   };
+  // }
+
+  function addItem(item) {
+    item._id = Math.random();
+    item.complete = false;
+    setList( [...list, item]);
+  };
+
+  function toggleComplete(id) {
+
+    let item = list.filter(i => i._id === id)[0] || {};
+
+    if (item._id) {
+      item.complete = !item.complete;
+      let listThings = list.map(listItem => listItem._id === item._id ? item : listItem);
+      setList(listThings);
+    }
+
+  };
+
+
+
+  
+
+  
     return (
       <>
         <header>
           <h2>
-          There are {this.state.list.filter(item => !item.complete).length} Items To Complete
+          There are {list.filter(item => !item.complete).length} Items To Complete
           </h2>
         </header>
 
         <section className="todo">
 
           <div>
-            <TodoForm handleSubmit={this.addItem} />
+            <TodoForm handleSubmit={addItem} />
           </div>
 
           <div>
             <TodoList
-              list={this.state.list}
-              handleComplete={this.toggleComplete}
+              list={list}
+              handleComplete={toggleComplete}
             />
           </div>
         </section>
       </>
     );
-  }
+  
 }
 
 export default ToDo;
